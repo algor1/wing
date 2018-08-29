90a254f25f7a01b52971291a646d96b5d9a74006
algor1 <alexeev.igor.v@gmail.com>
2018-08-29 13:34:15 +0300

    first

---
+++ camera/CamControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour {
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float cameraZoom = Input.GetAxisRaw ("camzoom");
+//		print (cam.transform.position);
+		if (cameraZoom!=0) {
+//			print(cam.transform.forward);
+			cam.transform.position = cam.transform.position+cam.transform.forward*cameraZoom;
+		}
+//		Vector3 rotVer = Vector3.up * x_rotate_ship;
+//		Vector3 rotHor = Vector3.right * y_rotate_ship;
+//		Vector3 rotZ = Vector3.forward * z_rotate_ship;
+//		Vector3 rotation = (rotHor+rotVer+rotZ).normalized*rotation_speed;
+//		Vector3 screenPos = cam.WorldToScreenPoint(target_tmp);
+//		Debug.Log("target is " + screenPos.x + " pixels from the left"+screenPos.y+" dist "+screenPos.z);
+		
+	}
+}
---
+++ player/TargetNameScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TargetNameScript : MonoBehaviour {
+	private Text tname;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		tname= GetComponent<Text> ();
+		tname.text = "no target";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.GetComponent<ShipMotor> ().thisShip.newtargetToMove!=null) {
+			tname.text = player.GetComponent<ShipMotor> ().thisShip.newtargetToMove.visibleName;
+//			print (player.GetComponent<ShipMotor> ().thisShip.newtargetToMove.visibleName);
+		} else {
+			tname.text = "no target";
+		}
+	}
+}
