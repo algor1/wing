2e438248611b2eab479d44c3a5a0aef5ca39ad61
algor1 <alexeev.igor.v@gmail.com>
2018-09-11 21:09:40 +0300

    missile

---
+++ missile/FireMissile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireMissile : MonoBehaviour {
+	
+	public GameObject missilePrefab;
+	public GameObject target;
+
+	public void Fire(){
+//		GameObject target = GameObject.Find ("Cube");
+		GameObject missile=(GameObject)Instantiate(missilePrefab,transform.position,transform.rotation);
+		missile.GetComponent<Missile>().target=target;
+	}
+}
