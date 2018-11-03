b242162856f3c791222e6ef79be9f27cf8fb3796
algor1 <alexeev.igor.v@gmail.com>
2018-11-04 00:48:17 +0300

    no message

---
+++ canvas/tmp_button.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tmp_button : MonoBehaviour {
+	[SerializeField]
+	private GameObject menuPrefab;
+
+	public void PressedTmpButton () {
+		GameObject someMenu = (GameObject)Instantiate(menuPrefab, gameObject.transform);
+//		someMenu.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+//		someMenu.transform.SetParent(gameObject.transform, false);
+		someMenu.GetComponent<CreateInventoryMenu>().leftHolder_id=0;
+		someMenu.GetComponent<CreateInventoryMenu>().leftHolder_id=200;
+//		return true;
+	}
+}
